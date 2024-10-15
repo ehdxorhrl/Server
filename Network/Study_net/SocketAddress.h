e8aec80c1c8743eaed1e6ab5701a578eef771310
ehdxorhrl <leedj6477@naver.com>
2024-10-15 11:59:07 +0900

    chapter 3.2

---
+++ Network/Study_net/SocketAddress.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Common.h"
+
+class SocketAddress
+{
+public:
+	SocketAddress(uint32_t inAddress, uint16_t inport)
+	{
+		GetAsSockAddrIn()->sin_family = AF_INET;
+		GetAsSockAddrIn()->sin_addr.S_un.S_addr = htonl(inAddress);
+		GetAsSockAddrIn()->sin_port = htonl(inport);
+	}
+
+	SocketAddress(const sockaddr& inSockAddr)
+	{
+		memcpy(&mSockAddr, static_cast<const sockaddr*>(&inSockAddr), sizeof(mSockAddr));
+	}
+
+private:
+	sockaddr mSockAddr;
+
+	sockaddr_in* GetAsSockAddrIn()
+	{
+		return reinterpret_cast<sockaddr_in*>(&mSockAddr);
+	}
+};
+
+using SocketAddressPtr = shared_ptr<SocketAddress>;
+
